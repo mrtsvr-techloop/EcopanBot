466e1fc74813b66748cede484ffad471c660e461
SaverioMartiradonna <201772461+mrtsvr-techloop@users.noreply.github.com>
2025-05-18 23:02:16 +0200

    frontend.js

---
+++ ecopan_bot/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+module.exports = {
+  entry: './public/js/App.jsx',          // tuo punto di ingresso React
+  output: {
+    path: path.resolve(__dirname, 'public/js'),
+    filename: 'frontend.js'               // bundle che verrà symlinkato
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env','@babel/preset-react']
+        }
+      }
+    }]
+  },
+  resolve: { extensions: ['.js','.jsx'] },
+  mode: 'production'
+};
